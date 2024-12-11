e518a4fec0c49713847631ac61f00040a6740532
Honza Piperek <janpiperek@gmail.com>
2024-12-11 11:46:33 +0100

    no message

--- tcpip/source/main-tcpip.cpp
+++ tcpip/source/main-tcpip.cpp
@@ -392,12 +392,12 @@ void task_blink_leds(void *tp_arg) {
                 else if(cmd.direction == RIGHT) {
                     for(int i = 0; i < cmd.num_leds && i < LED_PTB_NUM; i++) {
                         int idx = LED_PTB_NUM -1 -i;
-                        ptb_bool[idx].state = true;
+                        ptc_bool[idx].state = true;
                         vTaskDelay(200 / portTICK_PERIOD_MS);
                     }
                     for(int i = 0; i < cmd.num_leds && i < LED_PTB_NUM; i++) {
                         int idx = LED_PTB_NUM -1 -i;
-                        ptb_bool[idx].state = false;
+                        ptc_bool[idx].state = false;
                         vTaskDelay(200 / portTICK_PERIOD_MS);
                     }
                 }
